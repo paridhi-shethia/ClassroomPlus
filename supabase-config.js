6a898dd7d4c47c7c8e49d197b4339e9e5082e76c
paridhi-shethia <paridhisethia04@gmail.com>
2026-04-15 15:50:40 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,3 @@
-const SUPABASE_URL = "https://ozuyxwvaeaktjoyxafui.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dXl4d3ZhZWFrdGpveXhhZnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjE0MTgsImV4cCI6MjA5MTM5NzQxOH0.c9gsAvnqeGsaJ9BhihpDz0NE9eJn3nKoQuo6yCIc3zQ";
-
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.SUPABASE_URL = "https://ozuyxwvaeaktjoyxafui.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dXl4d3ZhZWFrdGpveXhhZnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjE0MTgsImV4cCI6MjA5MTM5NzQxOH0.c9gsAvnqeGsaJ9BhihpDz0NE9eJn3nKoQuo6yCIc3zQ";
+window.db = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
