f7946816acec7d71721a6206aebda32a731fdd6c
paridhi-shethia <paridhisethia04@gmail.com>
2026-04-15 16:00:23 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,1 @@
-window.SUPABASE_URL = "https://ozuyxwvaeaktjoyxafui.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dXl4d3ZhZWFrdGpveXhhZnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjE0MTgsImV4cCI6MjA5MTM5NzQxOH0.c9gsAvnqeGsaJ9BhihpDz0NE9eJn3nKoQuo6yCIc3zQ";
-window.db = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
+window.db = supabase.createClient("https://ozuyxwvaeaktjoyxafui.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dXl4d3ZhZWFrdGpveXhhZnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjE0MTgsImV4cCI6MjA5MTM5NzQxOH0.c9gsAvnqeGsaJ9BhihpDz0NE9eJn3nKoQuo6yCIc3zQ");
